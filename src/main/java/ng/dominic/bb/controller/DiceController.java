bbda27457383163f6f0f1d47bd02c02f3c3448b3
dng <dominicnzl@hotmail.com>
2020-12-10 19:26:51 +0100

    wrapped DiceController Results in ResponseEntity

--- src/main/java/ng/dominic/bb/controller/DiceController.java
+++ src/main/java/ng/dominic/bb/controller/DiceController.java
@@ -5,11 +5,14 @@ import ng.dominic.bb.util.Dice;
 import ng.dominic.bb.util.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 public class DiceController {
@@ -23,31 +26,36 @@ public class DiceController {
     }
 
     @GetMapping("/roll/{chosenDice}")
-    public Result rollSpecific(@PathVariable String chosenDice) {
+    public ResponseEntity<Result> rollSpecific(@PathVariable String chosenDice) {
         chosenDice = chosenDice == null || chosenDice.isEmpty()
                 ? "D6"
                 : chosenDice;
         var d = diceMap.get(chosenDice);
         logger.info("Dice created was: {}", d);
-        return new Result(d);
+        var result = new Result(d);
+        return ok(result);
     }
 
     @GetMapping("/choices")
-    public Set<String> diceChoices() {
-        return diceMap.keySet();
+    public ResponseEntity<Set<String>> diceChoices() {
+        return diceMap.keySet().isEmpty()
+                ? noContent().build()
+                : ok(diceMap.keySet());
     }
 
     @PostMapping(value = "/roll")
-    public Result roll(@RequestBody String json) {
+    public ResponseEntity<Result> roll(@RequestBody String json) {
         try {
             List<String> diceNames = JsonPath.read(json, "$.dice[*]");
             var dice = diceNames.stream()
                     .map(diceMap::get)
                     .toArray(Dice[]::new);
-            return new Result(dice);
+            var result = new Result(dice);
+            return ResponseEntity.ok(result);
         } catch (Exception e) {
             logger.warn("Input argument did not parse correctly, returned a D6 Result: {}", e.getMessage());
-            return new Result(diceMap.get("d6"));
+            var defaultResult = new Result(diceMap.get("d6"));
+            return ResponseEntity.badRequest().body(defaultResult);
         }
     }
 }
